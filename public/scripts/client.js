f0d680517a69a1ea38c5990d65a5bde610c4fbad
Sharon Lee <sharonshlee@icloud.com>
2021-09-07 18:14:41 -0400

    add click new-icon to create new tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
+$(document).ready(function () {
+  $(".new-icon").on("click", function (event) {
+    console.log("focus");
+    $("#tweet-text").focus();
+  });
+});
