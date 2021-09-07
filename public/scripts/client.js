d1c149e1768209e642c1a4aad98d6e1ebdb0bfda
Sharon Lee <sharonshlee@icloud.com>
2021-09-07 18:40:09 -0400

    edit new-icon click to scroll top

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
+  // Click on new-icon to create new tweet
   $(".new-icon").on("click", function (event) {
-    console.log("focus");
     $("#tweet-text").focus();
+    $(document).scrollTop(0); //position top:0
   });
 });
