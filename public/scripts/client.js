398b92659b999bf649b924a3c5621867581d875a
Sharon Lee <sharonshlee@icloud.com>
2021-09-07 20:07:40 -0400

    add dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,9 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function () {
-  // Click on new-icon to create new tweet
-  $(".new-icon").on("click", function (event) {
-    $("#tweet-text").focus();
-    $(document).scrollTop(0); //position top:0
-  });
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  const createTweetElement = function (tweet) {
+    let timeAgo = timeago.format(tweet.created_at);
+    let tweetArticle = $(`
+        <article class="tweet">
+        <header>
+          <div class="image-name">
+            <img src=${tweet.user.avatars}/>
+            <span>${tweet.user.name}</span>
+          </div>
+          <div class="lastname">${tweet.user.handle}</div>
+        </header>
+
+        <main>${tweet.content.text}</main>
+
+        <footer>
+          <span>${timeAgo}</span>
+          <div class="icons">
+            <span class="fas fa-flag flag-icon hover-cursor"></span>
+            <span class="fas fa-retweet retweet-icon hover-cursor"></span>
+            <span class="fas fa-heart love-icon hover-cursor"></span>
+          </div>
+        </footer>
+        </article>
+    `);
+
+    return tweetArticle;
+  };
+
+  const renderTweets = function (tweets) {
+    for (const tweet of tweets) {
+      // calls createTweetElement for each tweet
+      const tweetArticle = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $(".tweets").append(tweetArticle);
+    }
+  };
+
+  renderTweets(data);
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -12,4 +12,10 @@ $(document).ready(function () {
       $(counter).css("color", "");
     }
   });
+
+  // Click on new-icon to create new tweet
+  $(".new-icon").on("click", function (event) {
+    $("#tweet-text").focus();
+    $(document).scrollTop(0); //position top:0
+  });
 });
