76af0359bbd332c1cea945a4fc0865ce56a63288
Sharon Lee <sharonshlee@icloud.com>
2021-09-08 15:46:12 -0400

    fetching tweets with ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,36 +3,9 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-// Fake data taken from initial-tweets.json
-// const data = [
-//   {
-//     user: {
-//       name: "Newton",
-//       avatars: "https://i.imgur.com/73hZDYK.png",
-//       handle: "@SirIsaac",
-//     },
-//     content: {
-//       text: "If I have seen further it is by standing on the shoulders of giants",
-//     },
-//     created_at: 1461116232227,
-//   },
-//   {
-//     user: {
-//       name: "Descartes",
-//       avatars: "https://i.imgur.com/nlhLi3I.png",
-//       handle: "@rd",
-//     },
-//     content: {
-//       text: "Je pense , donc je suis",
-//     },
-//     created_at: 1461113959088,
-//   },
-// ];
-
 const createTweetElement = function (tweet) {
-  let timeAgo = timeago.format(tweet.created_at);
-  let tweetArticle = $(`
+  const timeAgo = timeago.format(tweet.created_at);
+  const tweetArticle = $(`
         <article class="tweet">
         <header>
           <div class="image-name">
@@ -59,33 +32,31 @@ const createTweetElement = function (tweet) {
 };
 
 const renderTweets = function (tweets) {
+  $(".tweets").empty(); //clear the previous tweets
+
   for (const tweet of tweets) {
     // calls createTweetElement for each tweet
     const tweetArticle = createTweetElement(tweet);
-    // takes return tweet article and prepend it to the tweets section
+    // takes return tweet article and append to the beginning of the tweets section
     $(".tweets").prepend(tweetArticle);
   }
 };
 
-const loadFirst = function () {
+const loadTweets = function () {
   $.get("/tweets", function (data) {
     renderTweets(data);
   });
 };
 
-//renderTweets(data);
-
-const loadTweets = function (newTweet) {
+const postTweet = function (newTweet) {
   $.post("/tweets", newTweet, function () {
-    $.get("/tweets", function (data) {
-      renderTweets(data);
-    });
+    loadTweets();
   });
 };
 
 $(document).ready(function () {
-  loadFirst();
-  // create an AJAX POST request that sends the form data to the server.
+  loadTweets();
+
   $("#frm-new-tweet").submit(function (event) {
     // prevent the default behaviour of the submit event (data submission and page refresh)
     event.preventDefault();
@@ -93,6 +64,10 @@ $(document).ready(function () {
     //Serialize the form data before posting to the server, key=value pair
     const newTweet = $(this).serialize();
 
-    loadTweets(newTweet);
+    //AJAX POST request that sends the form data to the server.
+    postTweet(newTweet);
+
+    //clear textarea after post and focus the textarea for new input
+    $("#tweet-text").val("").focus();
   });
 }); //close ready
